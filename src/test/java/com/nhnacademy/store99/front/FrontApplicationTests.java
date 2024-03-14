f97d9e9bbadc4d434439a226e63efec3e601f196
Song Ahyeon <ahyeonsong2001@gmail.com>
2024-03-14 09:47:26 +0900

    init

---
+++ src/test/java/com/nhnacademy/store99/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.store99.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
