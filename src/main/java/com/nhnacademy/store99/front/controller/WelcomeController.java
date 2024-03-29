fa4d6d778cba52d7a8cfd238eb0a0f00f0091484
rosin23 <rosinessrosin@gmail.com>
2024-03-29 18:38:09 +0900

    [feat] 코드 재사용성을 위한 공통 부분 작성

--- src/main/java/com/nhnacademy/store99/front/controller/WelcomeController.java
+++ src/main/java/com/nhnacademy/store99/front/controller/WelcomeController.java
@@ -8,7 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class WelcomeController {
-
+    /*
+    @Bean
+    public layoutDialect layoutDialect() {
+        return new layoutDialect();
+    }
+*/
     @GetMapping("/")
     public String index(HttpServletRequest request, Model model){
         String ip = request.getHeader("x-forwarded-for");
