fbaa6a0f2a5f06120fbf05e0125ad5f6129727e5
김승규 <rlatmdrb1996@gmail.com>
2024-04-01 17:29:19 +0900

    feat: Add totalCount in CommonListResponse

--- src/main/java/com/nhnacademy/store99/front/common/CommonListResponse.java
+++ src/main/java/com/nhnacademy/store99/front/common/CommonListResponse.java
@@ -6,10 +6,12 @@ import lombok.Builder;
 public class CommonListResponse<T> {
     private final CommonHeader header;
     private final List<T> resultList;
+    private final int totalCount;
 
     @Builder
-    private CommonListResponse(CommonHeader header, List<T> resultList) {
+    private CommonListResponse(CommonHeader header, List<T> resultList, int totalCount) {
         this.header = header;
         this.resultList = resultList;
+        this.totalCount = totalCount;
     }
 }
