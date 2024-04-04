579a4ea2bd85f2cd4f1af2da3d4f897010de1777
yrrho2 <dyrho22@naver.com>
2024-04-04 20:23:46 +0900

    feat: 위즈윅 사용X html에서 도서정보 post 가능.
    Resolves: #23

--- src/main/java/com/nhnacademy/store99/front/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/store99/front/book/controller/BookController.java
@@ -1,9 +1,15 @@
 package com.nhnacademy.store99.front.book.controller;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * 도서 뷰 컨트롤러
@@ -22,5 +28,21 @@ public class BookController {
     public String viewBookSalesPage(@PathVariable Long id, Model model) {
         model.addAttribute("bookId", id);
         return "book/book_sales_page";
+    }
+
+    @GetMapping("/books/admin")
+    public String viewManageBookDetail() {
+        return "book/book_admin";
+    }
+
+
+    @ResponseBody
+    @PostMapping(value = "/books/admin")
+    public String postBook(@RequestBody String bookRequest) {
+        System.out.println(bookRequest);
+        JsonParser parser = new JsonParser();
+        JsonObject object = parser.parse(bookRequest).getAsJsonObject();
+        System.out.println(object);
+        return "index";
     }
 }
