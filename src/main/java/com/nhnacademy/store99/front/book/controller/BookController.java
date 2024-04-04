e3e143a0f0c74bddb12b0e1932021103565419f6
yrrho2 <dyrho22@naver.com>
2024-04-04 21:11:11 +0900

    refactor: 오타 수정

--- src/main/java/com/nhnacademy/store99/front/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/store99/front/book/controller/BookController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -30,7 +29,6 @@ public class BookController {
     }
 
 
-    @ResponseBody
     @PostMapping(value = "/books/admin")
     public String postBook(@RequestBody String bookRequest) {
         System.out.println(bookRequest);
