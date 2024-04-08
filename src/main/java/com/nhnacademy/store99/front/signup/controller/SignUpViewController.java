25d76382ad2683992ccf0a66c9a4935a2b57c438
jinhyogyeom <jdkk1541@naver.com>
2024-04-08 09:47:30 +0900

    sign-up

--- src/main/java/com/nhnacademy/store99/front/signup/controller/SignUpViewController.java
+++ src/main/java/com/nhnacademy/store99/front/signup/controller/SignUpViewController.java
@@ -1,2 +1,16 @@
-package com.nhnacademy.store99.front.signup.controller;public class SignupViewController {
+package com.nhnacademy.store99.front.signup.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class SignUpViewController {
+    @GetMapping("/sign-up")
+    public String SignUpView() {
+        return "sign-up";
+    }
+
 }
