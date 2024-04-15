70c2bacff5ad3ea123ceee4ab951f9f54c70f6e9
김승규 <rlatmdrb1996@gmail.com>
2024-04-15 13:05:57 +0900

    comment: Add AdminPermissionDeniedException javadoc

--- src/main/java/com/nhnacademy/store99/front/admin/exception/AdminPermissionDeniedException.java
+++ src/main/java/com/nhnacademy/store99/front/admin/exception/AdminPermissionDeniedException.java
@@ -1,5 +1,10 @@
 package com.nhnacademy.store99.front.admin.exception;
 
+/**
+ * 관리자 권한이 없을 때 발생하는 에러
+ *
+ * @author seunggyu-kim
+ */
 public class AdminPermissionDeniedException extends RuntimeException {
     public AdminPermissionDeniedException() {
         super("관리자 권한이 없습니다.");
