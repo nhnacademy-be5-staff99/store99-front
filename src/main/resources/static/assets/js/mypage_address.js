48a0e75bbd4fc908cb9219dc74d7d5c9a61f42ec
Song Ahyeon <ahyeonsong2001@gmail.com>
2024-05-01 13:34:29 +0900

    feat: 카카오 도로명주소 api 활용한 주소 검색 및 추가
    #135

--- src/main/resources/static/assets/js/mypage_address.js
+++ src/main/resources/static/assets/js/mypage_address.js
@@ -30,3 +30,34 @@ $(document).ready(function () {
         });
     });
 });
+
+// 주소 추가
+$(document).ready(function() {
+    $('#addAddress').click(function() {
+        var addressGeneral = $('#generalAddressInput').val();
+        var addressDetail = $('#detailAddressInput').val();
+        var addressAlias = $('#aliasInput').val();
+        var addressCode = $('#postCodeInput').val();
+
+        $.ajax({
+            url: '/address/add',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                addressGeneral: addressGeneral,
+                addressDetail: addressDetail,
+                addressAlias: addressAlias,
+                addressCode: addressCode
+            }),
+            success: function() {
+                // 성공적으로 요청이 완료되면 모달을 닫고 페이지를 새로 고침합니다.
+                $('#addressModal').modal('hide');
+                location.reload();
+            },
+            error: function() {
+                // 요청이 실패하면 오류 메시지를 표시합니다.
+                window.alert('주소 추가에 실패했습니다.');
+            }
+        });
+    });
+});
