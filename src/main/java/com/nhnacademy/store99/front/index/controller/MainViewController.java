bd5bffce76d150b3fe9e4946f3bda7615f82c044
yrrho2 <dyrho22@naver.com>
2024-05-07 16:09:14 +0900

    feat: 인덱스 페이지 수정

--- src/main/java/com/nhnacademy/store99/front/index/controller/MainViewController.java
+++ src/main/java/com/nhnacademy/store99/front/index/controller/MainViewController.java
@@ -1,13 +1,25 @@
 package com.nhnacademy.store99.front.index.controller;
 
+import com.nhnacademy.store99.front.book.Response.BookPageResponse;
+import com.nhnacademy.store99.front.book.service.BookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainViewController {
+    private final BookService bookService;
 
     @GetMapping({"/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model, Pageable pageable) {
+        List<BookPageResponse> list = bookService.getBooks(pageable).getContent();
+
+        model.addAttribute("bestBooksList", list);
+
         return "index";
     }
 }
