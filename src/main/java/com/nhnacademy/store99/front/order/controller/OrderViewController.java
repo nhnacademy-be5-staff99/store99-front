b745157739fed2ea12b987c33e5f2167f078f9e2
김승규 <rlatmdrb1996@gmail.com>
2024-05-14 13:02:48 +0900

    fix: 토스 페이먼츠 결제 위젯 -> SDK 변경
    Resolves: https://github.com/nhnacademy-be5-staff99/store99-front/issues/170

--- src/main/java/com/nhnacademy/store99/front/order/controller/OrderViewController.java
+++ src/main/java/com/nhnacademy/store99/front/order/controller/OrderViewController.java
@@ -1,11 +1,8 @@
 package com.nhnacademy.store99.front.order.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.nhnacademy.store99.front.order.dto.request.OrderBookRequest;
 import com.nhnacademy.store99.front.order.dto.response.BookInOrderResponse;
 import com.nhnacademy.store99.front.order.service.OrderQueryService;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -15,20 +12,18 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.http.ResponseEntity;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author seunggyu-kim
@@ -46,7 +41,7 @@ public class OrderViewController {
         model.addAttribute("orderId", UUID.randomUUID().toString());
         model.addAttribute("orderBookList", orderBookList);
         model.addAttribute("totalProductPrice",
-                orderBookList.stream().mapToInt(BookInOrderResponse::getBookPrice).sum());
+                orderBookList.stream().mapToInt(BookInOrderResponse::getBookSalePrice).sum());
         model.addAttribute("orderName", getOrderName(orderBookList));
         return isLogin ? "order/checkout_main_member" : "order/checkout_main_guest";
     }
@@ -65,93 +60,74 @@ public class OrderViewController {
         return orderBookList.get(0).getBookTitle() + " 외 " + (orderBookList.size() - 1) + "권";
     }
 
-    /**
-     * 인증성공처리
-     *
-     * @param request
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(value = "/success", method = RequestMethod.GET)
-    public String paymentRequest(HttpServletRequest request, Model model) throws Exception {
-        return "order/success";
-    }
+    @GetMapping(value = "success")
+    public String paymentResult(Model model, @RequestParam(value = "orderId") String orderId,
+                                @RequestParam(value = "amount") Integer amount,
+                                @RequestParam(value = "paymentKey") String paymentKey) throws Exception {
 
-    /**
-     * 인증실패처리
-     *
-     * @param request
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(value = "/fail", method = RequestMethod.GET)
-    public String failPayment(HttpServletRequest request, Model model) throws Exception {
-        String failCode = request.getParameter("code");
-        String failMessage = request.getParameter("message");
+        String secretKey = "test_sk_Z1aOwX7K8m12DZ9omOOq8yQxzvNP:";
 
-        model.addAttribute("code", failCode);
-        model.addAttribute("message", failMessage);
-
-        return "order/fail";
-    }
-
-    @RequestMapping(value = "/confirm")
-    public ResponseEntity<ObjectNode> confirmPayment(@RequestBody String jsonBody) throws Exception {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String orderId;
-        String amount;
-        String paymentKey;
-        try {
-            // 클라이언트에서 받은 JSON 요청 바디입니다.
-            Map<String, Object> requestData = objectMapper.readValue(jsonBody, Map.class);
-            paymentKey = (String) requestData.get("paymentKey");
-            orderId = (String) requestData.get("orderId");
-            amount = (String) requestData.get("amount");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        ObjectNode obj = objectMapper.createObjectNode();
-        obj.put("orderId", orderId);
-        obj.put("amount", amount);
-        obj.put("paymentKey", paymentKey);
-
-        // TODO: 개발자센터에 로그인해서 내 결제위젯 연동 키 > 시크릿 키를 입력하세요. 시크릿 키는 외부에 공개되면 안돼요.
-        // @docs https://docs.tosspayments.com/reference/using-api/api-keys
-        String widgetSecretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
-
-        // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용하지 않습니다.
-        // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가합니다.
-        // @docs https://docs.tosspayments.com/reference/using-api/authorization#%EC%9D%B8%EC%A6%9D
         Base64.Encoder encoder = Base64.getEncoder();
-        byte[] encodedBytes = encoder.encode((widgetSecretKey + ":").getBytes(StandardCharsets.UTF_8));
+        byte[] encodedBytes = encoder.encode(secretKey.getBytes(StandardCharsets.UTF_8));
         String authorizations = "Basic " + new String(encodedBytes);
 
-        // 결제 승인 API를 호출하세요.
-        // 결제를 승인하면 결제수단에서 금액이 차감돼요.
-        // @docs https://docs.tosspayments.com/guides/payment-widget/integration#3-결제-승인하기
-        URL url = new URL("https://api.tosspayments.com/v1/payments/confirm");
+        URL url = new URL("https://api.tosspayments.com/v1/payments/" + paymentKey);
+
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestProperty("Authorization", authorizations);
         connection.setRequestProperty("Content-Type", "application/json");
         connection.setRequestMethod("POST");
         connection.setDoOutput(true);
+        JSONObject obj = new JSONObject();
+        obj.put("orderId", orderId);
+        obj.put("amount", amount);
 
         OutputStream outputStream = connection.getOutputStream();
-        outputStream.write(objectMapper.writeValueAsString(obj).getBytes(StandardCharsets.UTF_8));
+        outputStream.write(obj.toString().getBytes(StandardCharsets.UTF_8));
 
         int code = connection.getResponseCode();
         boolean isSuccess = code == 200;
+        model.addAttribute("isSuccess", isSuccess);
 
         InputStream responseStream = isSuccess ? connection.getInputStream() : connection.getErrorStream();
 
-        // TODO: 결제 성공 및 실패 비즈니스 로직을 구현하세요.
         Reader reader = new InputStreamReader(responseStream, StandardCharsets.UTF_8);
-        ObjectNode jsonObject = objectMapper.readValue(reader, ObjectNode.class);
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(reader);
         responseStream.close();
+        model.addAttribute("responseStr", jsonObject.toJSONString());
+        System.out.println(jsonObject.toJSONString());
 
-        return ResponseEntity.status(code).body(jsonObject);
+        model.addAttribute("method", jsonObject.get("method"));
+        model.addAttribute("orderName", jsonObject.get("orderName"));
+
+        if (jsonObject.get("method") != null) {
+            if (jsonObject.get("method").equals("카드")) {
+                model.addAttribute("cardNumber", ((JSONObject) jsonObject.get("card")).get("number"));
+            } else if (jsonObject.get("method").equals("가상계좌")) {
+                model.addAttribute("accountNumber",
+                        ((JSONObject) jsonObject.get("virtualAccount")).get("accountNumber"));
+            } else if (jsonObject.get("method").equals("계좌이체")) {
+                model.addAttribute("bank", ((JSONObject) jsonObject.get("transfer")).get("bank"));
+            } else if (jsonObject.get("method").equals("휴대폰")) {
+                model.addAttribute("customerMobilePhone",
+                        ((JSONObject) jsonObject.get("mobilePhone")).get("customerMobilePhone"));
+            }
+        } else {
+            model.addAttribute("code", jsonObject.get("code"));
+            model.addAttribute("message", jsonObject.get("message"));
+        }
+
+        return "order/success";
+    }
+
+    @GetMapping(value = "fail")
+    public String paymentResult(Model model, @RequestParam(value = "message") String message,
+                                @RequestParam(value = "code") Integer code) throws Exception {
+
+        model.addAttribute("code", code);
+        model.addAttribute("message", message);
+
+        return "order/fail";
     }
 }
