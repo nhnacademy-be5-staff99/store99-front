fbcef170559911ad984130b2fe8e6419f9c7435b
김승규 <rlatmdrb1996@gmail.com>
2024-05-16 00:40:15 +0900

    fix: mixed content

--- src/main/java/com/nhnacademy/store99/front/order/controller/OrderRestController.java
+++ src/main/java/com/nhnacademy/store99/front/order/controller/OrderRestController.java
@@ -3,6 +3,8 @@ package com.nhnacademy.store99.front.order.controller;
 import com.nhnacademy.store99.front.order.dto.request.ConfirmPaymentRequest;
 import com.nhnacademy.store99.front.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderRestController {
     private final OrderService orderService;
 
-    @PostMapping("/order")
+    @PostMapping("/orders")
     public void confirmPayment(@RequestBody ConfirmPaymentRequest confirmPaymentRequest) {
         orderService.confirmPayment(confirmPaymentRequest);
+    }
+
+    @DeleteMapping("/orders/{orderId}")
+    public void undoPendingPayment(@PathVariable String orderId) {
+        orderService.undoPayment(orderId);
     }
 
 }
